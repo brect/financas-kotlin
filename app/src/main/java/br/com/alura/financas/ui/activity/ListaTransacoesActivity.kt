1e8ae3a3c267e5cdf46b1b5a47df63db8edb3060
brect <b.limas93@gmail.com>
2019-12-17 21:02:16 -0300

    Utilizando o synthetic para buscar as views;

---
+++ app/src/main/java/br/com/alura/financas/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,23 @@
+package br.com.alura.financas.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import br.com.alura.financas.R
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+
+        val transacoes = listOf("Comida - R$ 20,50",
+                            "Economias - R$ 100,00")
+
+        val arrayArrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
+
+        lista_transacoes_listview.setAdapter(arrayArrayAdapter)
+    }
+
+}
